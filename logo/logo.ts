98c5b5a63d50c3583509c410ae880ef4f49b1452
Marcelo Bosso <m.bosso@ymail.com>
2019-06-19 13:20:40 -0300

    qol

--- logo/logo.ts
+++ logo/logo.ts
@@ -1,21 +1,21 @@
 export function Logo() {
-    let i = ["%c{black}.%c{}.        . `  .: . :. : .  . :.  .  . . .     . .    '    '' .  .  . ..  ",
-"%c{black}.%c{}   . .   :` . :   .  .'.' '....xxxxx...,'. '   ' .     ..",
-"%c{black}.%c{}; . ` .  . : . .' :  . ..XXXXXXXXXXXXXXXXXXXXx.    `     . ",
-".    .  .  . . .   .  ..XXXXXXXXWWWWWWWWWWWWWWWWXXX.  .     .     ",
-"%c{black}.%c{}   ' :  : . : .  ...XXXXXWWW'   W88N88@888888WWWWWXX.   .   .       . .",
-". ' .    . :   ...XXXXXXWWW'    M88N88GGGGGG888^8M 'WMBX.          .   ..  :",
-"%c{black}.%c{}    :     ..XXXXXXXXWWW'     M88888WWRWWWMW8oo88M   WWMX.     .    :    .",
-"%c{black}.%c{}      'XXXXXXXXXXXXWW'       WN8888WWWWW  W8@@@8M    BMBRX.         .  : :",
-".      XXXXXXXX=MMWW':  .      W8N888WWWWWWWW88888W      XRBRXX.  .       .",
-"%c{black}.%c{}....  ''XXXXXMM::::. .        W8@889WWWWWM8@8N8W      . . :RRXx.    .",
-"%c{black}.%c{}    ``...'''  MMM::.:.  .      W888N89999888@8W      . . ::::'RXV    .  :",
-".       ..'''''   MMMm::.  .      WW888N88888WW     .  . mmMMMMMRXx",
-"%c{black}.%c{} ..' .            ''MMmm .  .       WWWWWWW   . :. :,miMM'''  : ''`    .",
-".                .       ''MMMMmm . .  .  .   ._,mMMMM'''  :  ' .  :",
-"%c{black}.%c{}          .                  ''MMMMMMMMMMMMM''' .  : . '   .        .",
-"%c{black}.%c{}     .              .     .    .                      .         .",
-".                                         .          .         ."];
+    let i = ["%c{black}.%c{}.  . `  .: . :. : .  . :.  .  . . .     . .    '    '' .  .  . ..  ",
+"%c{black}.%c{}.  :` . :   .  .'.' '....xxxxx...,'. '   ' .     ..",
+"%c{black}.%c{} ` . : . .' :  . ..XXXXXXXXXXXXXXXXXXXXx.    `     . ",
+".    .  . . .   ..XXXXXXXXWWWWWWWWWWWWWWWWXX.  .     .     ",
+"%c{black}.%c{}' : . : .  ...XXXXXWWW'  W88N88@888888WWWWWXX.   .   .       . .",
+". ' .   :...XXXXXXWWW'  M88N88GGGGGG88888M 'WMBX.          .   ..  :",
+"%c{black}.%c{}:  ..XXXXXXXXWWW'     M88888WWRWWW W8oo88M   WWMX.     .    :    .",
+"%c{black}.%c{} XXXXXXXXXXXXWW'       WN8888WWWW   WW8@@@8M    BMBRX.         .  : :",
+".XXXXXXXX=MMWW':  .      W8N888WWWW ,WW88888W      XRBRXX.  .       .",
+"%c{black}.%c{}.'XXXXXMM::::. .        W8@889WWWWWM8@8N8W    . . :RRXx.    .",
+"%c{black}.%c{} ``.''  MMM::.:.  .      W888N89999888@8W   . . ::::'RXV    .  :",
+".      ..'''MMMm::.  .      WW888N88888WW  .  . mmMMMMMRXx",
+"%c{black}.%c{}.'.        ''MMmm .  .       WWWWWWW   . :. :,miMM'''  : ''`    .",
+".             .   ''MMMMmm . .  .  .   ._,mMMMM'''  :  ' .  :",
+"%c{black}.%c{}    .                  ''MMMMMMMMMMMMM''' .  : . '   .        .",
+"%c{black}.%c{}  .           .     .    .                      .         .",
+".                                   .          .         ."];
     return i;
 }
 
